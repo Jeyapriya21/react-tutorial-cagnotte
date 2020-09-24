e01720b26a48e5a5f5a2873fdbdb55b50f110a79
michael <nguyen.hoang.michael@gmail.com>
2020-09-24 08:55:15 +0200

    Adding factory method

--- src/App.js
+++ src/App.js
@@ -39,30 +39,9 @@ class App extends React.Component {
         })
     }
 
-    handleGoEcran52 = () => {
+    handleGoEcran5Dynamic = (valeur) => () => {
         this.setState({
-            montant: 2,
-            numeroEcran: 5
-        })
-    }
-
-    handleGoEcran55 = () => {
-        this.setState({
-            montant: 5,
-            numeroEcran: 5
-        })
-    }
-    
-    handleGoEcran510 = () => {
-        this.setState({
-            montant: 10,
-            numeroEcran: 5
-        })
-    }
-
-    handleGoEcran520 = () => {
-        this.setState({
-            montant: 20,
+            montant: valeur,
             numeroEcran: 5
         })
     }
@@ -116,10 +95,10 @@ class App extends React.Component {
                     Félicitations, votre êtes loggés !<br />
                     Veuillez sélectionner un montant ci-dessous pour la cagnotte de votre ami(e) :
                 </p>
-                <button onClick={this.handleGoEcran52}>2 €</button>
-                <button onClick={this.handleGoEcran55}>5 €</button>
-                <button onClick={this.handleGoEcran510}>10 €</button>
-                <button onClick={this.handleGoEcran520}>20 €</button><br />
+                <button onClick={this.handleGoEcran5Dynamic(2)}>2 €</button>
+                <button onClick={this.handleGoEcran5Dynamic(5)}>5 €</button>
+                <button onClick={this.handleGoEcran5Dynamic(10)}>10 €</button>
+                <button onClick={this.handleGoEcran5Dynamic(20)}>20 €</button><br />
                 <button onClick={this.handleGoEcran4}>Autre ...</button>
             </div>
         );
